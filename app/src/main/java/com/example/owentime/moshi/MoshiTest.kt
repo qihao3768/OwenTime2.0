df7ec5e2b26ec65d866766f903ab13605513427f
DESKTOP-2D4TOD8\admin <wyyjkunkun@163.com>
2022-06-15 22:29:48 +0800

    123

--- app/src/main/java/com/example/owentime/moshi/MoshiTest.kt
+++ app/src/main/java/com/example/owentime/moshi/MoshiTest.kt
@@ -1,17 +1,38 @@
 package com.example.owentime.moshi
+import com.squareup.moshi.JsonClass
 
-class MoshiTest {
+import com.squareup.moshi.Json
+import com.squareup.moshi.Moshi
 
+
+fun main(){
     val json="""{
   "code": 1000,
   "message": "success",
   "data": {
-    "photo": "https://owen-time-test.oss-cn-beijing.aliyuncs.com/user/1628239902_418ccb3dc145f105.png"
+    "photo": "https"
   },
   "status_code": 200
 }"""
+    val moShi=Moshi.Builder().build()
+    val m=moShi.adapter(MoShi::class.java).fromJson(json)
+    println(m)
 }
 
-class MyModel{
+@JsonClass(generateAdapter = true)
+data class MoShi(
+    @Json(name = "code")
+    var code: Int? = 0,
+    @Json(name = "data")
+    var data: Data? = Data(),
+    @Json(name = "message")
+    var message: String? = "",
+    @Json(name = "status_code")
+    var statusCode: Int? = 0
+)
 
-}
+@JsonClass(generateAdapter = true)
+data class Data(
+    @Json(name = "photo")
+    var photo: String? = ""
+)
