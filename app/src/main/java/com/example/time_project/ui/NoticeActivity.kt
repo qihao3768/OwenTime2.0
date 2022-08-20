2c581c78b4715752e1df087e3649dde7c911f852
yangjie <wyyjkunkun@163.com>
2022-08-20 15:12:35 +0800

    123

--- app/src/main/java/com/example/time_project/ui/NoticeActivity.kt
+++ app/src/main/java/com/example/time_project/ui/NoticeActivity.kt
@@ -1,7 +1,9 @@
 package com.example.time_project.ui
 
 import android.content.Intent
+import android.os.Build
 import android.provider.Settings
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationManagerCompat
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.time_project.App
@@ -13,15 +15,16 @@ import com.example.time_project.fastClick
 
 class NoticeActivity : BaseActivity(R.layout.activity_notice) {
     private val mBinding by viewBinding(ActivityNoticeBinding::bind)
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun initData() {
         mBinding.noticeLeft.fastClick {
             finish()
         }
         mBinding.tvGoSetting.fastClick {
             val intent:Intent= Intent()
-            intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
-            intent.putExtra(Settings.EXTRA_APP_PACKAGE, getPackageName())
-            intent.putExtra(Settings.EXTRA_CHANNEL_ID, getApplicationInfo().uid)
+            intent.action = Settings.ACTION_APP_NOTIFICATION_SETTINGS
+            intent.putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+            intent.putExtra(Settings.EXTRA_CHANNEL_ID, applicationInfo.uid)
             startActivity(intent)
         }
     }
